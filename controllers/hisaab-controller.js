38a5052ca77e14a1887f5d745151f8909ec490c9
the-sahil-gupta <sahilgtech95@gmail.com>
2024-07-24 11:58:35 +0530

    completed

--- controllers/hisaab-controller.js
+++ controllers/hisaab-controller.js
@@ -35,11 +35,57 @@ module.exports.createHisaabController = async function (req, res) {
 };
 module.exports.viewHisaabController = async function (req, res) {
 	try {
-		let hisaab = await hisaabModel
+		const hisaab = await hisaabModel
 			.findOne({ _id: req.params.id })
 			.populate('user');
+		if (!hisaab) return res.redirect('/profile');
+		if (hisaab.encrypted)
+			return res.render('passcode', {
+				loggedin: false,
+				id: req.params.id,
+			});
 		res.render('hisaab', { hisaab, loggedin: false });
 	} catch (err) {
 		res.send(err.message);
 	}
+};
+module.exports.deleteHisaabController = async function (req, res) {
+	const id = req.params.id;
+	const hisaab = await hisaabModel.findOne({ _id: id, user: req.user.id });
+
+	if (!hisaab) return res.redirect('/profile');
+
+	await hisaabModel.deleteOne({ _id: id });
+	return res.redirect('/profile');
+};
+module.exports.editHisaabController = async function (req, res) {
+	const hisaab = await hisaabModel.findOne({
+		_id: req.params.id,
+		user: req.user.id,
+	});
+
+	res.render('edit', { hisaab, loggedin: false });
+};
+module.exports.editPostHisaabController = async function (req, res) {
+	const hisaab = await hisaabModel.findOne({
+		_id: req.params.id,
+		user: req.user.id,
+	});
+	if (!hisaab) return res.redirect('/profile');
+
+	hisaab.title = req.body.title;
+	hisaab.description = req.body.description;
+	hisaab.editpermissions = req.body.editpermissions === 'on' ? true : false;
+	hisaab.shareable = req.body.shareable === 'on' ? true : false;
+	hisaab.encrypted = req.body.encrypted === 'on' ? true : false;
+	hisaab.passcode = req.body.passcode;
+
+	await hisaab.save();
+
+	res.redirect('/profile');
+};
+module.exports.verifyHisaabController = async function (req, res) {
+	const hisaab = await hisaabModel.findOne({ _id: req.params.id });
+	if (hisaab.passcode !== req.body.passcode) return res.redirect('/profile');
+	else return res.render('hisaab', { hisaab });
 };
--- routes/hisaab-router.js
+++ routes/hisaab-router.js
@@ -5,6 +5,10 @@ const {
 	createHisaabController,
 	hisaabPageController,
 	viewHisaabController,
+	deleteHisaabController,
+	editHisaabController,
+	editPostHisaabController,
+	verifyHisaabController,
 } = require('../controllers/hisaab-controller');
 
 const {
@@ -14,6 +18,12 @@ const {
 
 router.get('/create', isLoggedIn, hisaabPageController);
 router.post('/create', isLoggedIn, createHisaabController);
-router.get('/view/:id', isLoggedIn, viewHisaabController);
 
+router.get('/view/:id', isLoggedIn, viewHisaabController);
+router.get('/delete/:id', isLoggedIn, deleteHisaabController);
+
+router.get('/edit/:id', isLoggedIn, editHisaabController);
+router.post('/edit/:id', isLoggedIn, editPostHisaabController);
+
+router.post('/verify/:id', isLoggedIn, verifyHisaabController);
 module.exports = router;
